e5c6f79d560e4ea09996d0a8dc82af7c30af74c2
giorgos <perakisgeorgios@gmail.com>
2018-03-17 17:20:03 +0200

    create create_baseline_mapper()

--- app/baseline.py
+++ app/baseline.py
@@ -1,1 +1,29 @@
-from app import data_fetcher
+from app.data_fetcher import DataFetcher
+import pandas as pd
+from collections import Counter
+
+
+def create_baseline_mapper():
+    """
+
+    :return:
+    """
+    data_dict = DataFetcher.read_data(files_list=['train', 'dev'])
+
+    train_data = DataFetcher.parse_conllu(data_dict['train'])
+    dev_data = DataFetcher.parse_conllu(data_dict['dev'])
+
+    out = list()
+    for sentence in train_data + dev_data:
+        if sentence:
+            for t in sentence:
+                x = t[0].strip().lower()
+                out.append((x, t[1]))
+
+    df = pd.DataFrame(out, columns=['word', 'pos_tag'])
+
+    out_dict = dict()
+    for w, group in df.groupby(['word']):
+        out_dict[w] = Counter(group['pos_tag']).most_common(1)[0][0]
+
+    return out_dict
