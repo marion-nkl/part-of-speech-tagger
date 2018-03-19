c0599bda3eb0e0eca7053155f6b22a08f4ff3b72
Angeliki Romanou <agromanou@gmail.com>
2018-03-19 16:19:47 +0200

    Implement CRF tagger

---
+++ app/pos_taggers.py
@@ -0,0 +1,68 @@
+from nltk.corpus import treebank
+from nltk.tag import hmm
+from nltk.tag import CRFTagger
+from app.data_fetcher import DataFetcher
+
+
+class Tagger:
+    def __init__(self, data, tagger_name):
+        self.train_data = data  # list
+        self.tagger = None
+        self.tagger_name = tagger_name
+
+    def fit(self):
+        """
+        Fits a tagging model to object's data based on object's tagger name
+        :return: a tagger object
+        """
+        tagger = None
+        if self.tagger_name == 'hmm':
+            # Setup a trainer with default(None) values
+            # And train with the data
+            trainer = hmm.HiddenMarkovModelTrainer()
+            tagger = trainer.train_supervised(self.train_data)
+
+        elif self.tagger_name == 'crf':
+            trainer = CRFTagger()
+            trainer.train(self.train_data, 'model.crf.tagger')
+            tagger = trainer
+
+        self.tagger = tagger
+
+if __name__ == '__main__':
+    data = treebank.tagged_sents()[:3000]
+    data_dict = DataFetcher.read_data()
+    train_data = DataFetcher.parse_conllu(data_dict['train'])
+    cleaned_train_data = DataFetcher.remove_empty_sentences(train_data)
+
+    hmm_obj = Tagger(cleaned_train_data, 'hmm')
+    hmm_obj.fit()
+
+    hmm_obj_nltk = Tagger(data, 'hmm')
+    hmm_obj_nltk.fit()
+
+    print('-'*30)
+    print('HMM Tagger')
+    print('Tagging with our dataset: {}'.format(hmm_obj.tagger.tag('This is a sentence'.split())))
+    print('Tagging with nltk corpus: {}'.format(hmm_obj_nltk.tagger.tag('This is a sentence'.split())))
+
+    print()
+    print()
+
+    crf_obj = Tagger(cleaned_train_data, 'crf')
+    crf_obj.fit()
+
+    crf_obj_nltk = Tagger(data, 'crf')
+    crf_obj_nltk.fit()
+
+    print('-'*30)
+    print('CRF Tagger')
+    print('Tagging with our dataset: {}'.format(crf_obj.tagger.tag_sents(['This is a sentence'.split()])))
+    print('Tagging with nltk corpus: {}'.format(crf_obj_nltk.tagger.tag_sents(['This is a sentence'.split()])))
+
+    print()
+    print()
+
+
+
+
