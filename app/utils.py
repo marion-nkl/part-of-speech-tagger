9e78cd1b156bd2c791a1c72cc07a5294e29edd8f
Perakis Georgios <perakisgeorgios@gmail.com>
2018-03-23 17:03:52 +0200

    added verbocity to feeders

--- app/utils.py
+++ app/utils.py
@@ -4,12 +4,13 @@ import numpy as np
 from numpy import random
 
 
-def feed_cross_validation(sentences, seed=1234, k_folds=5):
+def feed_cross_validation(sentences, seed=1234, k_folds=5, verbose=0):
     """
     This method feeds the train and held_out data-sets in each iteration.
     :param sentences: list. An iterable of sentences
     :param seed: Int. A number that helps in the reproduction of the shuffling
     :param k_folds: Int. Number of folds.
+    :param verbose: Int. Defines the level of verbosity.
     :return: yields a dict of train and held_out data-sets.
     """
 
@@ -17,7 +18,7 @@ def feed_cross_validation(sentences, seed=1234, k_folds=5):
     np.random.seed(seed)
 
     # shuffling the list of sentences.
-    print('Shuffling data set in order to brake to train and held_out data-sets')
+    if verbose > 0: print('Shuffling data set in order to brake to train and held_out data-sets')
     random.shuffle(sentences)
 
     # calculating the ratios in actual numbers
@@ -25,9 +26,11 @@ def feed_cross_validation(sentences, seed=1234, k_folds=5):
 
     # split_size
     split_size = int(total_len / float(k_folds))
-    print('Splitting data-set in {} folds'.format(k_folds))
-    print('Split size for held out dataset: {}'.format(split_size))
-    print('Split size for training dataset: {}'.format(total_len - split_size))
+
+    if verbose > 0:
+        print('Splitting data-set in {} folds'.format(k_folds))
+        print('Split size for held out dataset: {}'.format(split_size))
+        print('Split size for training dataset: {}'.format(total_len - split_size))
 
     for i in range(1, k_folds + 1):
         yield {'held_out': sentences[(i - 1) * split_size: i * split_size],
@@ -46,10 +49,7 @@ def feed_crf_params(grid_search_params=None):
             'c2': [1e-3, 1e-2, 1e-1],  # coefficient for L2 penalty
             'max_iterations': [50, 100, 200],  # stop earlier
             'feature.possible_transitions': [True, False]  # include transitions that are possible, but not observed
-
         }
-
-    print(grid_search_params)
 
     # sorting the keys
     sorted_keys = sorted(grid_search_params)
@@ -59,5 +59,4 @@ def feed_crf_params(grid_search_params=None):
     # for each combination get the params in a dict
     for i in combinations:
         params = dict(zip(sorted_keys, i))
-
         yield params
