7d6fb9fc51fe1127a97238b878239af94cc41d9c
Perakis Georgios <perakisgeorgios@gmail.com>
2018-03-23 16:04:27 +0200

    created feed_crf_params()

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,5 @@
+import itertools as it
+
 import numpy as np
 from numpy import random
 
@@ -30,3 +32,32 @@ def feed_cross_validation(sentences, seed=1234, k_folds=5):
     for i in range(1, k_folds + 1):
         yield {'held_out': sentences[(i - 1) * split_size: i * split_size],
                'train': sentences[:(i - 1) * split_size] + sentences[i * split_size:]}
+
+
+def feed_crf_params(grid_search_params=None):
+    """
+    This function provides all the combinations of a greedy search parameters.
+    :param grid_search_params:
+    :return: dict. A dictionary of parameters for the crf model.
+    """
+    if grid_search_params is None:
+        grid_search_params = {
+            'c1': [1.0, 0.1, 0.01],  # coefficient for L1 penalty
+            'c2': [1e-3, 1e-2, 1e-1],  # coefficient for L2 penalty
+            'max_iterations': [50, 100, 200],  # stop earlier
+            'feature.possible_transitions': [True, False]  # include transitions that are possible, but not observed
+
+        }
+
+    print(grid_search_params)
+
+    # sorting the keys
+    sorted_keys = sorted(grid_search_params)
+    # creating all the possible combinations for all the parameters
+    combinations = it.product(*(grid_search_params[key] for key in sorted_keys))
+
+    # for each combination get the params in a dict
+    for i in combinations:
+        params = dict(zip(sorted_keys, i))
+
+        yield params
