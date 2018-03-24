02d54f7b3848f136cd1d1fb44d3cad8521ed25c0
giorgos <perakisgeorgios@gmail.com>
2018-03-25 00:01:19 +0200

    created baseline_benchmark() and create_baseline_benchmark_plot()

--- app/baseline.py
+++ app/baseline.py
@@ -1,7 +1,10 @@
 from collections import Counter
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from matplotlib.font_manager import FontProperties
+from sklearn.metrics import f1_score, accuracy_score
 
 from app.data_fetcher import DataFetcher
 from app.evaluation import create_report
@@ -80,7 +83,6 @@ def main(n_rarest_words=2500):
 
     # predicting the pos tags in order to calculate the accurasy and the classification report
     test_tuples_df['y_pred'] = baseline.predict(test_tuples_df['word'])
-
     # extracting the classes of the tags in order to pass it for the classification report.
     classes = sorted(set(test_tuples_df['y_pred']) | set(test_tuples_df['y_pred']))
 
@@ -107,7 +109,7 @@ def main(n_rarest_words=2500):
     to_plot = x.sort_values('y_true', ascending=False)['y_true'] * 100
     to_plot.reset_index(drop=True, inplace=True)
     to_plot.plot(title='Words Frequency from Most Common to Rarest', grid=True)
-    plt.ylabel('Percentage of Occurances')
+    plt.ylabel('Percentage of Occurrences')
     plt.show()
 
     # creating a bar plot for each pos tag by calculating the occurrences for each one tag.
@@ -119,5 +121,130 @@ def main(n_rarest_words=2500):
     plt.show()
 
 
+def baseline_benchmark(train, test):
+    """
+    This function calculates metrics for evaluation of a Baseline POS TAGGER classifier over a training and a test set.
+
+    :param train: list
+    :param test: list
+    :return: dict
+    """
+    baseline = Baseline()
+    # fitting both the train data
+    baseline.fit(X=train)
+
+    # creating a dataframe with the test words and the true (actual) pos tags.
+    test_tuples_df = pd.DataFrame(DataFetcher.pre_processing(test), columns=['word', 'y_true'])
+
+    # predicting the pos tags in order to calculate the accuracy and the classification report
+    test_tuples_df['y_pred'] = baseline.predict(test_tuples_df['word'])
+    test_tuples_df['y_pred'].fillna(baseline.model['most_common_tag'], inplace=True)
+
+    y_true = test_tuples_df['y_true']
+    y_pred = test_tuples_df['y_pred']
+
+    if y_pred.isnull().sum() > 0:
+        print(test_tuples_df[test_tuples_df['y_pred'].isnull()])
+
+    accuracy = accuracy_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred, average='weighted')
+
+    return {'accuracy': accuracy, 'f1': f1}
+
+
+def create_baseline_benchmark_plot(train,
+                                   test,
+                                   n_splits=20,
+                                   plot_outfile=None,
+                                   y_ticks=0.025,
+                                   min_y_lim=0.4):
+    """
+
+    :param train:
+    :param test:
+    :param n_splits:
+    :param plot_outfile:
+    :param y_ticks:
+    :param min_y_lim: 
+    :return:
+    """
+
+    results = {'train_size': [], 'on_test': [], 'on_train': []}
+
+    split_size = int(len(train) / n_splits)
+
+    # setting parameters for the graph.
+    font_p = FontProperties()
+
+    font_p.set_size('small')
+
+    fig = plt.figure()
+    fig.suptitle('Baseline POS Tagger Learning Curves', fontsize=20)
+
+    ax = fig.add_subplot(111)
+    ax.axis(xmin=0, xmax=len(train) * 1.05, ymin=0, ymax=1.1)
+
+    plt.xlabel('N. of training instances', fontsize=18)
+    plt.ylabel('Accuracy', fontsize=16)
+
+    plt.grid(True)
+
+    plt.axvline(x=int(len(train) * 0.3))
+    plt.yticks(np.arange(0, 1.025, 0.025))
+
+    if y_ticks == 0.05:
+        plt.yticks(np.arange(0, 1.025, 0.05))
+
+    elif y_ticks == 0.025:
+        plt.yticks(np.arange(0, 1.025, 0.025))
+
+    plt.ylim([min_y_lim, 1.025])
+
+    # each time adds up one split and refits the model.
+    counter = split_size
+    for i in range(n_splits):
+        train_x_part = train[:counter]
+        counter += split_size
+
+        print(20 * '*')
+        print('Split {} size: {}'.format(i, len(train_x_part)))
+
+        results['train_size'].append(len(train_x_part))
+
+        result_on_test = baseline_benchmark(train=train_x_part, test=test)
+
+        # calculates each time the metrics also on the test.
+        results['on_test'].append(result_on_test['accuracy'])
+
+        # calculates the metrics for the given training part
+        result_on_train_part = baseline_benchmark(train=train_x_part, test=train_x_part)
+
+        results['on_train'].append(result_on_train_part['accuracy'])
+
+        line_up, = ax.plot(results['train_size'], results['on_train'], 'o-', label='Accuracy on Train')
+
+        line_down, = ax.plot(results['train_size'], results['on_test'], 'o-', label='Accuracy on Test')
+
+        plt.legend([line_up, line_down], ['Accuracy on Train', 'Accuracy on Test'], prop=font_p)
+
+    if plot_outfile:
+        fig.savefig(plot_outfile)
+
+    plt.show()
+
+    return results
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+
+    # fetches and creates a dict containing the train, dev and test data.
+    data_dict = DataFetcher.read_data(files_list=['train', 'dev', 'test'])
+
+    train_data = DataFetcher.parse_conllu(data_dict['train'])
+    dev_data = DataFetcher.parse_conllu(data_dict['dev'])
+    test_data = DataFetcher.parse_conllu(data_dict['test'])
+
+    train_dev_data = train_data + dev_data
+
+    create_baseline_benchmark_plot(train=train_dev_data, test=test_data)
